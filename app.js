0b2d01391ff31e2889fe806f9d036fc09b965e33
EzequielGomezCucchiararo <ezequiel.cucchiararo@gmail.com>
2017-02-23 10:11:11 +0100

    pug layouts working well

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-
 const app = express()
 
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Tasks List')
+  const title = 'This is the layout from home'
+  res.render('layout', {title})
 })
 
 app.get('/completed', (req, res) => {
-  res.send('Tasks completed')
+  const title = 'This is the layout from completed'
+  res.render('layout', {title})
 })
 
 app.listen(3000, () => console.log('Listening ont PORT 3000'))
